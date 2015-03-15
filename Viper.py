888174a74e31d24772a6e63c874d5433b8cebff0
B3nac <Rockapparatus@yahoo.com>
2015-03-14 20:13:11 -0700

    Trying to add terminal colors

--- Viper.py
+++ Viper.py
@@ -26,19 +26,23 @@ print('Viper Alpha 0.0.5\n'
 print ('Welcome to Viper command terminal. Type help for list of commands.')
 #I had to use Python 2.7 becuase some of the modules weren't cross compatable.
 
+class tcolors:
+    WARNING = '\033[93m'
+    ENDC = '\033[0m'
+    
 class Vipercmd(cmd.Cmd):
     #Command example
-    
+
     def do_greet(self, person):
         if person:
-            print('hi',person)
+            print tcolors.WARNING + "hi" ,person + tcolors.ENDC
         else:
-            print('hi')
-		  
+            print tcolors.WARNING + "hi" + tcolors.ENDC
+            
     def do_help(self, commands):
         print('Current list of commands: greet, portscan, honeypot,'
               ' listcd, dirchange, dl, listf, remf, remdir, readf, runf, sechash, b64, exit')
-    
+
     #Security Functions.
     #---------------------------------------------------------------------
     def do_portscan(server_address, self):
@@ -55,15 +59,15 @@ class Vipercmd(cmd.Cmd):
                     return
         except socket.error:
             print "Couldn't connect to server"
-		
+
     def do_honeypot(sock, server_address):
-       	
+
        sock = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
        server_address = ('', 443)
        try:
            sock.bind(server_address)
        except socket.error:
-           print("Socket error, sudo run Viper or change ports.")   
+           print("Socket error, sudo run Viper or change ports.")
        else:
            while True:
                print >>sys.stderr, 'starting up on %s port %s' % sock.getsockname()
@@ -85,13 +89,13 @@ class Vipercmd(cmd.Cmd):
                            f.write(str(client_address))
                            f.write(str(compname))
                            f.close()
- 
+
                        else:
                            break
-                 
+
                finally:
                    connection.close()
-                   
+
     def do_netping(self, ping):
         print('Pinging network ips')
         for ping in range(1,10):
@@ -103,9 +107,9 @@ class Vipercmd(cmd.Cmd):
                 print('no response from', ip)
             else:
                 print('pinging', ip, 'NO!')
-	
+
 	#File and Navigation functions --------------------------------------
-			
+
     def do_dl(web, files):
        print('Downloads file to Viper directory.')
        try:
@@ -122,11 +126,11 @@ class Vipercmd(cmd.Cmd):
         print('If executing C or C++ program put ./ for Bash or Perl sh in front of file name.')
         #C and Bash work so far.
         f = subprocess.call(raw_input("Which file would you like to run? :"), shell=True)
-           		
+
     def do_listcd(self, cd):
 	    cd = os.getcwd()
 	    print(cd)
-		
+
     def do_dirchange(self, mow):
         cd = os.getcwd()
         print(cd)
@@ -143,11 +147,11 @@ class Vipercmd(cmd.Cmd):
                   'Did you include the entire path?')
         else:
             print('Directory changed to', mow)
-	    
+
     def do_listf(self, files):
         mypath = os.getcwd()
         print(os.listdir(mypath))
-		
+
     def do_remf(self, F):
         F = raw_input("What file would you like to remove? :")
         try:
@@ -156,7 +160,7 @@ class Vipercmd(cmd.Cmd):
             print('File does not exist.')
         else:
             print('File removed!')
-    
+
     def do_remdir(self, F):
         print('WARNING: This command deletes the whole directory and contents.')
         F = raw_input("What directory would you like to remove? :")
@@ -175,12 +179,12 @@ class Vipercmd(cmd.Cmd):
         except IOError:
             print('File not found.')
         #IOError
-        
+
     def do_overwrite(self, yeeee):
         #Shred function in the works.
         yo = Overwrite.Mystuff()
         yo.overwrite()
-    	
+
     def do_sechash(self, passwd):
         riot = raw_input('Input password. :')
         passwd = pbkdf2_sha256.encrypt(riot, rounds=200000, salt_size = 16)
@@ -190,27 +194,26 @@ class Vipercmd(cmd.Cmd):
         #False?
         f = open("passwd.txt", "a")
         f.write(str(passwd))
-    
+
 	    #ValueError: Empty mode string.
     def do_b64(self, passwd):
         riot = raw_input('Input password to encrypt. :')
         woh = base64.b64encode(riot)
         print(woh)
-        
+
     def do_decryptb64(self, decrypt):
         decode = raw_input('Input password to decrypt. :')
         decrypt = base64.b64decode(decode)
-        print(decrypt)    
-	    
+        print(decrypt)
+
     def do_EOF(self, line):
         return True
-        
+
     def do_exit(self, ex):
         ex = exit()
-		    
+
     def postloop(self):
         print()
 
 if __name__ == '__main__':
     Vipercmd().cmdloop()
-
