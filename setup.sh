1b3d885f858994d77a5d94d4d73f1f8229b6357e
b3nac <rockapparatus@yahoo.com>
2015-09-19 18:04:42 +0000

    Added setup.sh

---
+++ setup.sh
@@ -0,0 +1,15 @@
+#!/bin/bash
+
+clear
+echo
+echo
+
+echo -e "\e[1;33mInstalling Filezilla.\e[0m"
+if [ -n "$(command -v apt-get)" ]; then
+    apt-get -y install python3
+
+elif [ -n "$(command -v pacman)" ]; then
+	pacman -S python
+else
+	echo "apt-get and pacman aren't the current package managers?"
+	exit 
