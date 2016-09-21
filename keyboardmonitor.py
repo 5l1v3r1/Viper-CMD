c09504a1e893e2e4ce71f8e9b4a17abe80ece6d4
B3nac <Rockapparatus@yahoo.com>
2016-09-20 22:55:16 -0700

    Updated comments

--- keyboardmonitor.py
+++ keyboardmonitor.py
@@ -1,15 +1,11 @@
-"""
-A simple example of hooking the keyboard on Linux using pyxhook
+#import libraries
 
-Any key pressed prints out the keys values, program terminates when spacebar is pressed
-"""
-
-#Libraries we need
 import pyxhook
 import time
 import sys
 
-#This function is called every time a key is presssed
+#Key press function
+
 def kbevent( event ):
     #print key info
     print(event)
@@ -18,17 +14,15 @@ def kbevent( event ):
     butts.close()
     
     #If the ascii value matches spacebar, terminate the while loop
+
     if event.Ascii == 32:
         global running
         running = False
 
 #Create hookmanager
 hookman = pyxhook.HookManager()
-#Define our callback to fire when a key is pressed down
 hookman.KeyDown = kbevent
-#Hook the keyboard
 hookman.HookKeyboard()
-#Start our listener
 hookman.start()
     
 #Create a loop to keep the application running
