cd42e09445af626533bf9feadd4dd7d078eca923
B3nac <Rockapparatus@yahoo.com>
2016-09-24 11:49:33 -0700

    Fixed print statement and version number.

--- commands.py
+++ commands.py
@@ -11,9 +11,9 @@ print(tc.tcolors.WARNING +'            --  $$$$$  $      $      '+ tc.tcolors.EN
 print(tc.tcolors.WARNING +'                $       $$$$  $      '+ tc.tcolors.ENDC)
 print(tc.tcolors.WARNING +'                $                    '+ tc.tcolors.ENDC)
 print('\n')
-print(tc.tcolors.SUCCESS +'Viper-CMD Alpha 1.0.1\n'
+print(tc.tcolors.SUCCESS +'Viper-CMD Alpha 1.0.2\n'
       'by B3nac'+ tc.tcolors.ENDC)
-print(tc.tcolors.SYNTAX +'Welcome to Viper command terminal. Type help for list of commands.'+ tc.tcolors.ENDC)
+print(tc.tcolors.SYNTAX +'Welcome to Viper command terminal. Type help for a list of commands.'+ tc.tcolors.ENDC)
 
 class Command(object):
 
